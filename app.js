a9dd7cf41808fb155b8e3cb98cab6fc359fbeb69
VSS\hoai.nguyen <trunghoaiitiuh@gmail.com>
2023-02-01 17:57:26 +0700

    lesson 56 60%

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+app.use(adminRoutes);
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
