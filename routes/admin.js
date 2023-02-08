4424474c004045dbf74703711d2a059172182e1e
VSS\hoai.nguyen <trunghoaiitiuh@gmail.com>
2023-02-08 17:16:42 +0700

    pug - temmplate engine

--- routes/admin.js
+++ routes/admin.js
@@ -10,16 +10,14 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  // console.log(req.body);
   products.push({ title: req.body.title });
   res.redirect('/');
 });
 
 exports.routes = router;
 exports.products = products;
-// module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -3,18 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  //use pug template
-  res.render('shop', { prods: adminData.products, docTitle: 'Shop' });
-
-  // use basic html file
-  // console.log(adminData);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
